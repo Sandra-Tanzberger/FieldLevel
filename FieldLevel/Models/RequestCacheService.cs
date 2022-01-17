89314b3d74aab0a2f722db550368384416f2f263
Sandra Tanzberger <sandraewell@hotmail.com>
2022-01-17 09:13:07 -0700

    add background service

---
+++ FieldLevel/Models/RequestCacheService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace FieldLevel.Models
+{
+    public interface IRequestCacheService
+    {
+        Task RefreshCacheAsync();
+        void RemoveCacheAsync();
+    }
+    public class RequestCacheService: IRequestCacheService
+    {
+        private readonly IDistributedCache _cache;
+        private readonly ILogger _logger;
+
+        public RequestCacheService(IDistributedCache cache, ILogger<RequestCacheService> logger)
+        {
+            _cache = cache;
+            _logger = logger;
+        }
+
+        public async Task RefreshCacheAsync()
+        {
+            var posts = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(Posts.GetCurrentPosts()));
+
+            await _cache.SetAsync("Posts", posts, new DistributedCacheEntryOptions());
+
+            _logger.LogInformation("Request Cache Service cache refreshed.");
+        }
+
+        public void RemoveCacheAsync()
+        {
+            _cache.Remove("Posts");
+        }
+
+    }
+}
---
+++ FieldLevel/Models/RequestCachingBackgroundService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+
+namespace FieldLevel.Models
+{
+    public class RequestCachingBackgroundService: BackgroundService
+    {
+        private readonly ILogger _logger;
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("?Request Caching Service started...");
+
+            try
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    //getposts and cache
+
+
+                    await Task.Delay(60000);
+                }
+            }
+            catch (TaskCanceledException ex)
+            {
+                _logger.LogWarning(ex.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Request Caching Service stopped.");
+            await base.StopAsync(cancellationToken);
+        }
+
+    }
+}
